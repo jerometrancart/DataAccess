fde77ebb0b9c3628ca9ab320f901669cbc904611
jerometrancart <jerome.trancart@gmail.com>
2023-04-28 07:49:58 +0200

    Insert and read done with Dapper micro ORM

---
+++ Personne.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DataAccess
+{
+    public class Personne
+    {
+        public int Id { get; }
+        public string Nom { get; set; } 
+        public string Prenom { get; set; }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
 ﻿using System.Data;
 using Microsoft.Data.SqlClient;
+using Dapper;
+using DataAccess;
 
 
 ///STRUCTURE DU LIEN GOOGLABLE + TRUSTSERVERCERTIFICATE EN DEHORS DE LA PROD
 SqlConnection connection = new SqlConnection("Server=localhost;Database=ApprendreCsharp;User Id=sa;Password=Passw0rd*;TrustServerCertificate=True");
 
-connection.Open();
+///5 - CREATE WITH DAPPER 
+/*connection.Execute(
+    "INSERT INTO Personnes(Nom, Prenom) VALUES(@Nom, @Prenom)",
+    new
+    {
+        Nom = "GRANDE",
+        Prenom = "Ariana"
+    }); */
+
+///6 - READ WITH DAPPER
+var personnes = connection.Query<Personne>("SELECT * FROM Personnes");
+foreach (var personne in personnes)
+{
+    System.Console.WriteLine($"{personne.Nom} {personne.Prenom}");
+}
+
+///PAS BESOIN D OUVRIR LA CONNECTION AVEC DAPPER
+/// connection.Open();
 
 try 
 {
@@ -55,10 +74,10 @@ try
     } */
 
     ///2 - READ
-    SqlCommand command = new SqlCommand(
-        "SELECT * FROM Personnes",
-        connection
-        );
+    // SqlCommand command = new SqlCommand(
+    //     "SELECT * FROM Personnes",
+    //     connection
+    //     );
     
     ///COMMANDE QUI RETOURNE UNE INSTANCE DE SQLDATAREADER
     /* SqlDataReader reader = command.ExecuteReader();
@@ -72,24 +91,32 @@ try
     ///3 - DATATABLES
 
     ///DECLARE TABLE
-    DataTable table = new DataTable("Personnes");
+    /*DataTable table = new DataTable("Personnes");
 
     ///COLONNES
     DataColumn id = new DataColumn("Id", typeof(int));
     DataColumn nom = new DataColumn("Nom", typeof(string));
     DataColumn prenom = new DataColumn("Prenom", typeof(string));
     table.Columns.AddRange(new[] { id, nom, prenom });
-    ///PRIMARY KEY
+    ///PRIMARY KEY (A COMMENTER SI PLUSIEURS ROWS A INSERER)
     table.PrimaryKey = new[] { id };
 
     ///INSTANCIE SQLDATAADAPTER SUR LA SQLCOMMAND ECRITE PLUS HAUT
     SqlDataAdapter adapter = new SqlDataAdapter(command);
     ///STOCKE DANS LA TABLE EN MEMOIRE
-    adapter.Fill(table);
-
     ///POSSIBLE DE VERIFIER AVEC DEBUGGER + POINT D'ARRET
-    
+    adapter.Fill(table);*/
 
+    ///4 - CREATE AND WRITE ROWS AVEC DATATABLES
+    /*
+    ///ECRIT UN ROW VIRTUEL
+    table.Rows.Add(0, "LEPONGE", "Bob");
+    ///SE SERT DE LA CLASSE SQLBULKCOPY POUR ECRIRE
+    SqlBulkCopy copy = new SqlBulkCopy(connection);
+    ///LUI INDIQUE LA TABLE DE DESTINATION
+    copy.DestinationTableName = "Personnes";
+    ///ECRIT
+    copy.WriteToServer(table);*/
 
 }
 catch (System.Exception)
